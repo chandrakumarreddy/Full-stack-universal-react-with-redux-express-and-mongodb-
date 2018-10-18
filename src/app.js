81fa58c17d3a2ecd08bac56b0cfa34949b2304d9
chandra <chandrapenugonda655@gmail.com>
2018-10-18 23:15:59 +0530

    using-middleware

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
 import { addTodo, deleteTodo, updateTodo } from "./actions/Todos";
 import reducer from "./reducers";
-// step-03 define a reducer
+
+//middleware usage in redux
+
+const middleware = [logger];
 
 // step-01 define a store
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(...middleware));
 
 //step-04 listening to store and changes to state update
 store.subscribe(() => console.log("state from redux", store.getState()));
-
-// step-02  create an action and dispatch a store
 
 // Create opeartion in crud
 
