e3bbbf1242989111307bddede3cdd363af8b9e57
chandra <chandrapenugonda655@gmail.com>
2018-10-18 22:41:12 +0530

    delete operation added in crud

--- src/app.js
+++ src/app.js
@@ -5,6 +5,21 @@ const reducer = function(state = { books: [] }, action) {
 	switch (action.type) {
 		case "ADD_TODO":
 			return { ...state, books: [...state.books, ...action.payload] };
+		case "REMOVE_TODO":
+			// let arrayIndex = state.books.forEach((book, index) => {
+			// 	if (book._id === action.payload) return index;
+			// });
+			// return {
+			// 	...state,
+			// 	books: [
+			// 		...state.books.slice(0, arrayIndex),
+			// 		...state.books.slice(arrayIndex + 1)
+			// 	]
+			// };
+			return {
+				...state,
+				books: state.books.filter(item => item._id !== action.payload)
+			};
 		default:
 			return state;
 	}
@@ -46,3 +61,7 @@ store.dispatch({
 		}
 	]
 });
+
+store.dispatch({ type: "REMOVE_TODO", payload: 3 });
+
+store.dispatch({ type: "REMOVE_TODO", payload: 2 });
